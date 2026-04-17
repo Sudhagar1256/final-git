ab4353624b9dec7f9326b9e2834841444fe3ab3b
sudhagar p <sudhagar092004@gmail.com>
2026-04-17 11:34:04 +0530

    Update java2.java

--- java2.java
+++ java2.java
@@ -1,6 +1,10 @@
 public class java2 {
     public static void main(String[] args) {
         System.out.println("mera tera");
+        int a=10;
+        int b=19;
+        int c =a+b;
+        System.out.println(c);
     }
     
 }
